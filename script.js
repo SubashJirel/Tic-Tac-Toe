d3fafb0cebdcb89315eb26fab4f103f239a8eac5
Subash Jirel <subashjrl@gmail.com>
2023-08-19 10:11:13 +0545

    completely changed how the game should work, removed the all the 8 divs from the html and made a module in js that will automatically make 8 divs when clicked on start game. Everything i put insde the modules and also created a factory function to create palyers

--- script.js
+++ script.js
@@ -6,24 +6,65 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   const formInformation = new FormData(form);
   const data = Object.fromEntries(formInformation);
-  // console.log(data);
   document.querySelector('.modal-wrapper').setAttribute('hidden', true);
   document.querySelector('.buttonContainer.hide').classList.remove('hide');
-  initializeGame(data);
+
+  // initializeGame(data);
+  game.start(data);
 });
 
-const initializeVaraibles = (data) => {
-  //change the string choice from form to number
-  data.choice = +data.choice;
-  data.board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-  data.player1 = 'X';
-  data.player2 = 'O';
-  data.round = 0;
-  data.currentPlayer = 'X';
-  data.gameOver = false;
-};
+const gameBoard = (function () {
+  let gameBoard = ['', '', '', '', '', '', '', '', ''];
 
-const initializeGame = (data) => {
-  initializeVaraibles(data);
-  console.log(data);
+  const render = () => {
+    let boardHTML = '';
+    gameBoard.forEach((square, index) => {
+      // square is the element of the array that is either "X" or "O" and index is just a number
+      boardHTML += `<div id=square-${index} class="square">${square}</div>`;
+    });
+    document.querySelector('.gameBoard').innerHTML = boardHTML;
+  };
+  return { render };
+})();
+
+const createPlayer = (naam, mark) => {
+  return { naam, mark };
 };
+const game = (function () {
+  let players = [];
+  let currentPlayerIndex;
+  let gameOver;
+
+  const start = (data) => {
+    players = [
+      // createPlayer(document.querySelector('#form1').value, 'X'),
+      // createPlayer(document.querySelector('#form1').value, 'O'),
+      //getting players name from the data collected from form
+      createPlayer(data.player1Name, 'X'),
+      createPlayer(data.player2Name, 'X'),
+    ];
+
+    currentPlayerIndex = 0;
+    gameOver = false;
+    console.log(players);
+    // console.log('started');
+    gameBoard.render();
+  };
+  return { start };
+})();
+
+// const initializeVaraibles = (data) => {
+//   //change the string choice from form to number
+//   data.choice = +data.choice;
+//   data.board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+//   data.player1 = 'X';
+//   data.player2 = 'O';
+//   data.round = 0;
+//   data.currentPlayer = 'X';
+//   data.gameOver = false;
+// };
+
+// const initializeGame = (data) => {
+//   initializeVaraibles(data);
+//   console.log(data);
+// };
